597b39f9c78a138d5211486a0eea89ad4ab376d5
BenjySFQ <benjamin77122@gmail.com>
2022-08-03 06:02:34 -0500

    solution skill

--- src/main/java/com/devskiller/controller/ItemController.java
+++ src/main/java/com/devskiller/controller/ItemController.java
@@ -1,14 +1,18 @@
 package com.devskiller.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.devskiller.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("/titles")
 public class ItemController {
-
-    public List<String> getTitles(Double rating) {
+    @Autowired
+    ItemService service;
+    @GetMapping("/{rating}")
+    public List<String> getTitles(@PathVariable("rating") Double rating) {
         //TODO
-        return null;
+        return service.getTitlesWithAverageRatingLowerThan(rating);
     }
 }
--- src/main/java/com/devskiller/service/ItemService.java
+++ src/main/java/com/devskiller/service/ItemService.java
@@ -1,12 +1,20 @@
 package com.devskiller.service;
 
 import com.devskiller.dao.ItemRepository;
+import com.devskiller.model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    @Autowired
+    ItemRepository repo;
 
     public ItemService(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
@@ -14,7 +22,11 @@ public class ItemService {
 
     public List<String> getTitlesWithAverageRatingLowerThan(Double rating) {
         //TODO
-        return null;
+            List<Item>lista=itemRepository.findItemsWithAverageRatingLowerThan(rating);
+            List<String>listaTitulos=new ArrayList<String>();
+            for (int i = 0; i < lista.size(); i++) {
+                listaTitulos.add(lista.get(i).getTitle());
+            }
+            return listaTitulos;
     }
-
 }
